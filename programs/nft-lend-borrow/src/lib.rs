e84b95feb6b1e74f4981d1a3c19c42ee1f36d9a3
rafaelbtcsol <156289525+rafaelbtcsol@users.noreply.github.com>
2024-01-12 13:46:28 +0700

    update program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("5FV2qJiEh7TmgaMNf88gnxqfUfGPjy2NjSSXPQA3pmE2");
 
 #[program]
 pub mod nft_lend_borrow {
